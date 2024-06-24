1c65109af8fce4df58b6176cfd3b988196ad05b1
Eric Li <behold0815@gmail.com>
2024-06-24 16:29:55 +0800

    shadowing

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ fn main() {
             .read_line(&mut input_str)
             .expect("The number you input is unreadable.");
 
-        let input_ok: u32 = match input_str.trim().parse() {
-            Ok(input_ok) => input_ok,
+        let input_str: u32 = match input_str.trim().parse() {
+            Ok(input_str) => input_str,
             Err(_) => {
                 println!(
                     "The number {:?} you input is not a number.",
@@ -24,11 +24,11 @@ fn main() {
             }
         };
 
-        match input_ok.cmp(&random_number) {
-            Ordering::Less => println!("The number {} you guess is too small.", input_ok),
-            Ordering::Greater => println!("The number {} you guess is too big.", input_ok),
+        match input_str.cmp(&random_number) {
+            Ordering::Less => println!("The number {} you guess is too small.", input_str),
+            Ordering::Greater => println!("The number {} you guess is too big.", input_str),
             Ordering::Equal => {
-                println!("The number {} you guess is correct.", input_ok);
+                println!("The number {} you guess is correct.", input_str);
                 break;
             }
         }
